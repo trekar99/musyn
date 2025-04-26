54dfe63ae576fc54bb20cf535c7d2c93756a11e9
trekar99 <101529212+trekar99@users.noreply.github.com>
2025-04-26 16:12:46 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+title = "Musyn - Music Synesthesia in Real-Time"
+description = """
+<p style='text-align: center'> Musyn: Real-time Music-to-Image Co-creation System</p> 
+<p style='text-align: center'> Germán Puerto - 2025</p> 
+<p style='text-align: center'> <a href='https://github.com/trekar99/musyn' target='_blank'>Musyn GitHub</a> </p>
+<p style='text-align: center'> To use it, simply upload your audio and click 'submit', or click one of the examples to load them. Read more at the links below. </p>
+"""
+
+
+#example_list = ['electronic.mp3', 'orchestra.wav']
