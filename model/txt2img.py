d8dba687ad0a46a82d14037bf89f9054af16f88a
trekar99 <101529212+trekar99@users.noreply.github.com>
2025-05-08 11:29:44 +0200

    Change VAE to TAESDXL

--- model/txt2img.py
+++ model/txt2img.py
@@ -1,26 +1,27 @@
-from diffusers import StableDiffusionXLPipeline, AutoencoderKL
+from diffusers import StableDiffusionXLPipeline, AutoencoderTiny
 from huggingface_hub import snapshot_download
-from torch import float16, manual_seed
-from random import randint
-
-# TODO download
-#from diffusers import AutoencoderKL
-'''vae = AutoencoderKL.from_pretrained(
-  'madebyollin/sdxl-vae-fp16-fix',
-  use_safetensors=True,
-  torch_dtype=torch.float16,
-).to('cuda')'''
+from torch import float16#, manual_seed
+#from random import randint
 
 class ImageGenerator():
   def __init__(self):
     self.model_config_path = self.get_model_config()
 
+    self.vae = AutoencoderTiny.from_pretrained(
+                                      'madebyollin/taesdxl',
+                                      use_safetensors=True,
+                                      torch_dtype=float16,
+                                    )
+    
     self.pipeline = StableDiffusionXLPipeline.from_single_file("model/models/sdxlturbo.safetensors",
                                                       config=self.model_config_path,
                                                       local_files_only=True,
                                                       revision="fp16",
                                                       variant="fp16",
-                                                      torch_dtype=float16)
+                                                      torch_dtype=float16,
+                                                      vae=vae
+                                                      )
+    
     self.pipeline.set_progress_bar_config(disable=True)
 
   def get_model_config(self):
@@ -31,11 +32,11 @@ class ImageGenerator():
     )
 
   def image_generator(self, prompt, inference=""):
-    generator = manual_seed(randint(0, 9007199254740991))
+    #generator = manual_seed(randint(0, 9007199254740991))
     return self.pipeline(prompt=prompt + inference,
                 num_inference_steps=1,
                 guidance_scale=0.0,
-                generator=generator
+                #generator=generator
                 ).images[0]
 
   '''pipeline = StableDiffusionXLPipeline.from_single_file("model/models/sdxlturbo.safetensors",
