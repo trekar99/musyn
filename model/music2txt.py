045ed65f1cd94135fd4139944acd35bfc40cff52
trekar99 <101529212+trekar99@users.noreply.github.com>
2025-05-10 12:40:47 +0200

    MusicCapsGenerator Class

--- model/music2txt.py
+++ model/music2txt.py
@@ -1,11 +1,16 @@
+from model.bart import BartCaptionModel
+from torch import load
+import config
 
+class MusicCapsGenerator(BartCaptionModel):
+  def __init__(self, max_length=128):
+    super().__init__(max_length)
 
-class MusicCapsGenerator():
+    self.pretrained_object = load(config.musiccaps_model, map_location='cpu')
+    self.state_dict = self.pretrained_object['state_dict']
+    self.load_state_dict(self.state_dict)
 
-
-
-
-
+    self.eval()
 
 # Print model's state_dict
 #print("Model's state_dict:")
